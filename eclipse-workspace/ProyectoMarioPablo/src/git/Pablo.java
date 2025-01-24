8a40def3a3b62d5c6b5de0d2c24679b74a290853
pabloiglesiasprieto <pablo.iglesias@iesnervion.es>
2025-01-24 13:07:07 +0100

    Calculadora

---
+++ eclipse-workspace/ProyectoMarioPablo/src/git/Pablo.java
@@ -0,0 +1,21 @@
+package git;
+
+public class Pablo {
+
+	public static int suma(int num1, int num2) {
+		return num1+num2;
+		
+	}
+	public static int resta(int num1, int num2) {
+		return num1-num2;
+		
+	}
+	public static int multiplicacion(int num1, int num2) {
+		return num1*num2;
+		
+	}
+	public static double division(int num1, int num2) {
+		return num1/num2;
+		
+	}
+}
